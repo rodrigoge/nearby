8a293b6ab30b05903dbc5406a6c0f218ecd751e3
Rodrigo Gouveia <rgestevao@gmail.com>
2025-03-08 17:14:11 -0300

    feat: starting simple API project

---
+++ api/src/main/java/com/api/nearby/NearbyApplication.java
@@ -0,0 +1,13 @@
+package com.api.nearby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyApplication.class, args);
+	}
+
+}
